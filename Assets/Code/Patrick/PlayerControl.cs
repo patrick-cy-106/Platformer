6c1dcc0d97aa09ed8929c0bb556704fbdd70ad9b
patrick-cy-106 <pc2720@nyu.edu>
2022-02-25 00:23:45 -0500

    title-level1-proto

--- Assets/Code/Patrick/PlayerControl.cs
+++ Assets/Code/Patrick/PlayerControl.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour
 {   
+    // Basic Movement Variables
     Rigidbody2D _rigidbody;
     int speed = 10;
     int jumpForce = 800;
 
+    // Jumping Variables
     public LayerMask groundLayer;
     public Transform feetPos;
     float groundCheckDist = 0.3f;
     bool isGrounded = false;
+
+    // Shooting Variables
+    public GameObject bulletPrefab;
+    public Transform gunPos;
+    int bulletForce = 1000;
+
+    // Death Mechanic Variables
+    bool isAlive = true;
 
     void Start()
     {
@@ -20,17 +31,35 @@ public class PlayerControl : MonoBehaviour
 
     private void FixedUpdate() 
     {
+        // Check if grounded
         isGrounded = Physics2D.OverlapCircle(feetPos.position, groundCheckDist, groundLayer);
+
+        // Death Mechanic
+        if(isAlive && (transform.position.y <-10))
+        {
+            isAlive = false;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
     }
 
     void Update()
     {
+        // Left, Right Movement
         float xSpeed = Input.GetAxis("Horizontal") * speed;
         _rigidbody.velocity = new Vector2(xSpeed, _rigidbody.velocity.y);
 
+        // Jumping
         if(isGrounded && Input.GetButtonDown("Jump"))
         {
             _rigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+
+        // Shooting
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject newBullet = Instantiate(bulletPrefab, gunPos.position, Quaternion.identity);
+            newBullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(bulletForce * transform.localScale.x, 0));
         }
     }
 }
